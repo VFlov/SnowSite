b761db5f6b2d3a1896740fbdbf6fc1cd5cf75037
VFlov <129610413+VFlov@users.noreply.github.com>
2025-02-25 00:09:58 +0500

    update

--- snowsite.client/src/App.vue
+++ snowsite.client/src/App.vue
@@ -1,20 +1,21 @@
 <template>
-  
-    <router-view @authenticated="checkAuth"></router-view>
-    
+
+  <router-view @authenticated="checkAuth"></router-view>
+
 </template>
-<script>export default {
-  methods: {
-    checkAuth() {
-      if (localStorage.getItem('token')) {
-        this.$router.push('/chat');
+<script>
+  export default {
+    methods: {
+      checkAuth() {
+        if (localStorage.getItem('token')) {
+          this.$router.push('/chat');
+        }
       }
+    },
+    created() {
+      this.checkAuth();
     }
-  },
-  created() {
-    this.checkAuth();
-  }
-};</script>
+  };</script>
 <style>
   #app {
     max-width: 1200px;
@@ -23,7 +24,6 @@
     background-color: #fff;
     box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
     border-radius: 10px;
-    font-family : 'Ubuntu', sans-serif;
+    font-family: 'Ubuntu', sans-serif;
   }
-  
- </style>
+</style>
--- snowsite.client/src/main.js
+++ snowsite.client/src/main.js
@@ -9,27 +9,45 @@ import YandexMap from './YandexMap.vue';
 import Examples from './Examples.vue';
 import VideoCall from './VideoCall.vue';
 import Valheim from './Valheim.vue';
-import AuthPage from './Auth.vue'
+import AuthPage from './AuthPage.vue'
 
 //const Home = { template: Card }
 //const SocialNetwork = { template: Social }
 
 const routes = [
-  { path: '/', name: 'card', component: Main },
+  //{ path: '/', name: 'card', component: Main },
   { path: '/home', redirect: '/' },
   { path: '/social', name: 'social', component: Social },
-  { path: '/map', name : 'map', component: YandexMap },
-  { path: '/examples', name : 'examples', component: Examples},
+  { path: '/map', name: 'map', component: YandexMap },
+  { path: '/examples', name: 'examples', component: Examples },
   { path: '/videocall', name: 'videocall', component: VideoCall },
   { path: '/valheim', name: 'valheim', component: Valheim },
+  { path: '/auth', component: AuthPage },
   {
+    path: '/chat',
+    component: Social,
+    beforeEnter: (to, from, next) => {
+      if (!localStorage.getItem('token')) {
+        next('/auth');
+      } else {
+        next();
+      }
+    }
+  },
+  { path: '/', redirect: '/auth' }
+  /*{
     path: '/auth', name: 'auth', component: AuthPage, beforeEnter: (to, from, next) => {
       if (!localStorage.getItem('token')) {
         next('/auth');
       } else {
         next();
       }
-    } }
+    }, beforeLeave: (to, from, next) => {
+      localStorage.removeItem('token');
+      next();
+    } 
+  },
+  { path: '/', redirect: '/auth' } */
   //{ path: '/:pathMatch(.*)*', component: NotFound },
 ];
 const router = createRouter({
