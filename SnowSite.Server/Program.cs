43bbb6b216ff26487b063b93a3bc5c8c5278a656
VFlov <129610413+VFlov@users.noreply.github.com>
2025-02-25 23:30:43 +0500

    Revert "update"
    This reverts commit 4dd7587c57df0179cbd8fd4f07953aa1eee30e54.

--- SnowSite.Server/Program.cs
+++ SnowSite.Server/Program.cs
@@ -34,7 +34,6 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IChatService, ChatService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
-
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll",
@@ -53,10 +52,6 @@ builder.Services.AddControllers();
 var app = builder.Build();
 app.UseCors("AllowAll");
 app.UseRouting();
-app.UseStaticFiles();
-app.UseHttpsRedirection();
-app.UseAuthentication();
-
 app.UseAuthorization();
 app.MapHub<CallHub>("/chatHub");
 
