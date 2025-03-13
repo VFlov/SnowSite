9be3967e681c797c0841a48c40be4fbc493a9361
VFlov <129610413+VFlov@users.noreply.github.com>
2025-03-13 11:15:40 +0500

    PiplineDONE

--- SnowSite.Server/Program.cs
+++ SnowSite.Server/Program.cs
@@ -53,7 +53,6 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             }
         };
     });
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddHttpContextAccessor();
